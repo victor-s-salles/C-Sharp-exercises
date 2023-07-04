61aac4aa00a69dec005554b99f41f11fd40912f5
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-07-04 14:28:47 -0300

    Exemplo 1

--- Fundamentos-e-recursos-C#/dia-03-Arrays-Funcoes-e-Excecoes/MyExpenses/Program.cs
+++ Fundamentos-e-recursos-C#/dia-03-Arrays-Funcoes-e-Excecoes/MyExpenses/Program.cs
@@ -1,10 +1,28 @@
 ﻿class Program
 {
-    public int[] expensesCost;
+   
 
     static void Main(string[] args)
     {
+        int[] expensesCost;
         Console.WriteLine("Entre com o número de despesas: ");
-        int numberOfExepenses = getNumberOfExpenses();
+        int numberOfExpenses = getNumberOfExpenses();
+
+        for (int i=0; i < numberOfExpenses; i++)
+            {
+              expensesCost[i] = getExpenseCostFromUser();
+            }
+
+        int totalExpenses = 0;
+
+        for (int i = 0; i < numberOfExpenses; i++)
+            {
+            totalExpenses += expensesCost[i];
+            }
+
+        Console.WriteLine("O total das despesas é: " + totalExpenses);
     }
+
+
+
 }
