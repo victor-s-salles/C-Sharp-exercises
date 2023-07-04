c412e17c62470fb699195dadf0a36503d6eec79e
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-07-04 14:24:09 -0300

    Iniciando estudo do dia

---
+++ Fundamentos-e-recursos-C#/dia-03-Arrays-Funcoes-e-Excecoes/MyExpenses/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    public int[] expensesCost;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com o número de despesas: ");
+        int numberOfExepenses = getNumberOfExpenses();
+    }
+}
